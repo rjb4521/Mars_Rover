699e9f977b96c87260a754ccf15073d2516fa0af
Richard Byron <rjb4521@outlook.com>
2021-03-11 13:34:46 -0500

    Add to git

---
+++ Mars_Rover/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MarsRover;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mars_Rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+               .AddSingleton<ILandingSurface, Plataeu>()
+               .AddSingleton<IRoverSquadManager, RoverSquadManager>()
+               .BuildServiceProvider();
+
+            // Add or change commands here
+            var commandCenter = new CommandCenter(serviceProvider);
+            commandCenter.SendCommand("5 5");
+            commandCenter.SendCommand("1 2 N");
+            commandCenter.SendCommand("LMLMLMLMM");
+            commandCenter.SendCommand("3 3 E");
+            commandCenter.SendCommand("MMRMMRMRRM");
+
+            Console.ReadKey();
+        }
+    }
+}
